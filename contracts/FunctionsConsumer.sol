6e4838a91b233290ca605d2041ac1737b97922d3
Ege Aybars Bozkurt <egeaybars1102@gmail.com>
2023-03-05 14:19:28 +0300

    helper functions: from address to string

--- contracts/FunctionsConsumer.sol
+++ contracts/FunctionsConsumer.sol
@@ -41,7 +41,7 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
     string calldata source,
     bytes calldata secrets,
     Functions.Location secretsLocation,
-    string[] calldata args,
+    string[] memory args,
     uint64 subscriptionId,
     uint32 gasLimit
   ) public onlyOwner returns (bytes32) {
@@ -91,4 +91,25 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
   function addSimulatedRequestId(address oracleAddress, bytes32 requestId) public onlyOwner {
     addExternalRequest(oracleAddress, requestId);
   }
+
+  // Helper function to convert address to string
+  // Converting to string is neccessary for the request to be sent to the oracle
+  // Args are passed as strings, so the address needs to be converted to a string first
+  // So that it can be checked against the msg.sender 
+  function toAsciiString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);            
+        }
+        return string(s);
+    }
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
 }
